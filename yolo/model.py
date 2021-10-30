1d1578f2981a322ce4b032673bb3205bea67b4ef
Kaspiaan <kjallinson@gmail.com>
2021-10-30 06:50:35 +0100

    Updated model creation to make it easier

--- yolo/model.py
+++ yolo/model.py
@@ -13,7 +13,7 @@ class Yolo:
         self.s = s
         self.input_shape = (448, 448, 3)
 
-    def create_arch(self):
+    def create_darknet(self):
 
         darknet = models.Sequential()
 
@@ -21,22 +21,20 @@ class Yolo:
 
             if type(i) == tuple:
                 if i == CNN_CONFIG[0]:
-                    darknet.add(self.create_conv2d_layer(filters=i[1], kernel_size=i[0], strides=i[2],
-                                                         activation=self.math_util.leaky_relu(0.1),
+                    darknet.add(self.create_conv2d_layer(kernel_size=i[0], filters=i[1], strides=i[2],
                                                          input_shape=self.input_shape))
                 else:
-                    darknet.add(self.create_conv2d_layer(filters=i[0], kernel_size=i[1], strides=i[2],
-                                                         activation=self.math_util.leaky_relu(0.1)))
+                    darknet.add(self.create_conv2d_layer(kernel_size=i[0], filters=i[1], strides=i[2]))
 
             elif type(i) == str:
                 darknet.add(self.create_maxpool2d_layer(pool_size=2, strides=2))
 
             elif type(i) == list:
                 for _ in range(i[2]):
-                    darknet.add(self.create_conv2d_layer(filters=i[0][0], kernel_size=i[0][1], strides=i[0][2],
-                                                         activation=self.math_util.leaky_relu(0.1)))
-                    darknet.add(self.create_conv2d_layer(filters=i[1][0], kernel_size=i[1][1], strides=i[1][2],
-                                                         activation=self.math_util.leaky_relu(0.1)))
+                    darknet.add(self.create_conv2d_layer(kernel_size=i[0][0], filters=i[0][1], strides=i[0][2]))
+                    darknet.add(self.create_conv2d_layer(kernel_size=i[1][0], filters=i[1][1], strides=i[1][2]))
+
+
 
         # Tired nonsene
         # S, B, C = split, num_boxes, 1
@@ -57,7 +55,7 @@ class Yolo:
 
         return darknet
 
-    def create_conv2d_layer(self, filters: int, kernel_size: int, strides: int, activation: MathUtil,
+    def create_conv2d_layer(self, filters: int, kernel_size: int, strides: int, activation: MathUtil = MathUtil().leaky_relu(0.1),
                             input_shape: (int, int, int) = None) -> layers.Conv2D:
         if input_shape:
             return layers.Conv2D(
@@ -87,5 +85,5 @@ class Yolo:
 
 if __name__ == "__main__":
     lol = Yolo(13)
-    model = lol.create_arch()
+    model = lol.create_darknet()
     model.summary()
