2fa8bab80798db316ac1d083033f6daf90a9fa70
Kaspiaan <kjallinson@gmail.com>
2021-10-30 06:51:18 +0100

    Added a bunch more new fun activation functions

--- yolo/math_utils.py
+++ yolo/math_utils.py
@@ -1,5 +1,31 @@
 from tensorflow.keras.backend import maximum
+import numpy as np
+
 
 class MathUtil:
     def leaky_relu(self, alpha):
-        return lambda val : maximum(alpha * val, val)
+        return lambda val: maximum(alpha * val, val)
+
+    def relu(self):
+        return lambda val: maximum(0, val)
+
+    def heaviside_step(self):
+        return lambda val: 1 if val > 0 else 0
+
+    def sigmoid_linear_unit(self):
+        return lambda val: val / ((np.e ** - val) + 1)
+
+    def exponential_linear_unit(self, alpha):
+        return lambda val: val if val > 0 else alpha * ((np.e ** val) - 1)
+
+    def hypobolic_tangent(self):
+        return lambda val: np.tanh(val)
+
+    def identity(self):
+        return lambda val: val
+
+    def sigmoid(self):
+        return lambda val: 1 / ((np.e ** - val) + 1)
+
+    def mish(self):
+        return lambda val: val * np.tanh(np.log((np.e ** val) - 1))
